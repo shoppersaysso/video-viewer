94568884d90315781c3c0c88803036739edbd709
Sara Hopper <sara.hopper@gmail.com>
2017-11-10 21:00:09 -0500

    finished video controller

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,4 +1,5 @@
 class API::VideosController < ApplicationController
+  before_action :set_video, only: [:show, :update, :destroy]
 
   def index
     videos = Video.all
@@ -19,9 +20,8 @@ class API::VideosController < ApplicationController
   end
 
   def show
-    video = Video.find(params[:id])
-    if video
-      render json: video, status: 200
+    if @video
+      render json: @video, status: 200
     else
       render json: {
         errors: {
@@ -31,7 +31,28 @@ class API::VideosController < ApplicationController
     end
   end
 
+  def update
+    if @video.update(video_params)
+      render json: @video, status: 200
+    else
+      render json: {
+        errors: {
+          messages: { video: "unable to update"}
+          }
+        }, status: 404
+    end
+  end
+
+  def destroy
+    @video.destroy
+    :no_content
+  end
+
   private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
 
   def video_params
     params.require(:video).permit(:title, :description, :youtube_video_id)
